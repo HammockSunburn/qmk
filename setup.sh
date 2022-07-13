cc7842ccbff9b306f9c4b964fa9fec125c4ed444
Hammock Sunburn <hammocksunburn@gmail.com>
2022-07-12 20:58:00 -0500

    Initial commit for my keyboards

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+mk_link() {
+    dest_dir=$1
+
+    if [ ! -e "$dest_dir" ]; then
+	local_dir=$2
+	ln -sf $local_dir $dest_dir
+    fi
+}
+
+KBDS=$HOME/git/qmk_firmware/keyboards
+
+mk_link \
+    $KBDS/preonic/keymaps/HammockSunburn \
+    $PWD/preonic
+
+
+mk_link \
+    $KBDS/planck/keymaps/HammockSunburn \
+    $PWD/planck
+
+mk_link \
+    $KBDS/gmmk/pro/rev1/ansi/keymaps/HammockSunburn \
+    $PWD/gmmk
+
+mk_link \
+    $KBDS/massdrop/ctrl/keymaps/HammockSunburn \
+    $PWD/ctrl
